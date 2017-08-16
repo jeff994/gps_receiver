659d45d10cb60f6184f1842027fb69118614a4c4
chengyuen93 <cedric_ong93@hotmail.com>
2017-08-16 14:16:33 +0800

    modified client.cpp and added a launch file and sh file

--- gpsd_client/src/client.cpp
+++ gpsd_client/src/client.cpp
@@ -4,6 +4,9 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <sensor_msgs/NavSatStatus.h>
 #include <libgpsmm.h>
+//-------------------------------------------------------------------------------------------
+#include <stdlib.h> //added by cheng16/8
+//-------------------------------------------------------------------------------------------
 
 using namespace gps_common;
 using namespace sensor_msgs;
@@ -11,6 +14,22 @@ using namespace sensor_msgs;
 class GPSDClient {
   public:
     GPSDClient() : privnode("~"), gps(NULL), use_gps_time(true), check_fix_by_variance(true) {}
+
+//----added by cheng 16/8-------------------------------------------------------------------------------
+    void run_gpsd() {
+      std::string host = "localhost";
+      int port = 2947;
+      std::string dev = "None";
+      privnode.getParam("host", host);
+      privnode.getParam("port", port);
+      privnode.getParam("dev", dev);
+      // ROS_INFO("run %d", port);
+      std::stringstream run_gpsd;
+      run_gpsd << "gpsd -S " << port << " " << dev;
+      ROS_INFO("%s", run_gpsd.str().c_str());
+      system(run_gpsd.str().c_str());
+    }
+//------------------------------------------------------------------------------------------------------
 
     bool start() {
       gps_fix_pub = node.advertise<GPSFix>("extended_fix", 1);
@@ -23,6 +42,13 @@ class GPSDClient {
       int port = 2947;
       privnode.getParam("host", host);
       privnode.getParam("port", port);
+
+//----added by cheng 16/8-------------------------------------------------------------------------------
+      
+      std::string dev = "None";
+      privnode.getParam("dev", dev);
+
+//------------------------------------------------------------------------------------------------------
 
       char port_s[12];
       snprintf(port_s, 12, "%d", port);
@@ -42,7 +68,13 @@ class GPSDClient {
       gps->query("w\n");
 #endif
 
-      if (resp == NULL) {
+//-------added by cheng16/8--------------------------------------------------------------------
+      privnode.deleteParam("host");
+      privnode.deleteParam("port");
+      privnode.deleteParam("dev");
+//---------------------------------------------------------------------------------------------
+
+      if (resp == NULL || dev == "None") {
         ROS_ERROR("Failed to open GPSd");
         return false;
       }
@@ -52,14 +84,22 @@ class GPSDClient {
     }
 
     void step() {
+      // ROS_INFO("step");
 #if GPSD_API_MAJOR_VERSION >= 5
-      if (!gps->waiting(1e6))
+      if (!gps->waiting(1e6)) {
+        ROS_WARN("GPS wait time more than 1s .. : !gps->waiting(1e6)");
         return;
+      }
 
+      // ROS_INFO("p = gps->read()");
       gps_data_t *p = gps->read();
+      
 #else
+      // ROS_INFO("p = gps->poll()");
       gps_data_t *p = gps->poll();
+      
 #endif
+      // ROS_INFO("process data");
       process_data(p);
     }
 
@@ -78,13 +118,19 @@ class GPSDClient {
     bool check_fix_by_variance;
 
     void process_data(struct gps_data_t* p) {
-      if (p == NULL)
+      if (p == NULL) {
+        ROS_WARN("GPS data is NULL!");
         return;
+      }
 
-      if (!p->online)
+      if (!p->online) {
+        ROS_WARN("GPS is not online");
         return;
+      }
 
+      // ROS_INFO("process_data_gps");
       process_data_gps(p);
+      // ROS_INFO("process_data_navsat");
       process_data_navsat(p);
     }
 
@@ -137,7 +183,7 @@ class GPSDClient {
         status.satellite_visible_snr[i] = p->ss[i];
 #endif
       }
-
+      // ROS_INFO("%d", STATUS_FIX);
       if ((p->status & STATUS_FIX) && !(check_fix_by_variance && isnan(p->fix.epx))) {
         status.status = 0; // FIXME: gpsmm puts its constants in the global
                            // namespace, so `GPSStatus::STATUS_FIX' is illegal.
@@ -189,32 +235,41 @@ class GPSDClient {
 
     void process_data_navsat(struct gps_data_t* p) {
       NavSatFixPtr fix(new NavSatFix);
-
+      // ROS_INFO("process navsat");
       /* TODO: Support SBAS and other GBAS. */
 
-      if (use_gps_time && !isnan(p->fix.time))
+      if (use_gps_time && !isnan(p->fix.time)) {
         fix->header.stamp = ros::Time(p->fix.time);
-      else
+        // ROS_INFO("use_gps_time && !isnan(p->fix.time");
+      }
+      else {
+        // ROS_INFO("fix->header.stamp");
         fix->header.stamp = ros::Time::now();
+      }
 
       /* gpsmm pollutes the global namespace with STATUS_,
        * so we need to use the ROS message's integer values
        * for status.status
        */
+      std::string fix_str;                //added by cheng 16/8--------------------------------------------------
       switch (p->status) {
         case STATUS_NO_FIX:
           fix->status.status = -1; // NavSatStatus::STATUS_NO_FIX;
+          fix_str = "GPS can't get a fix";  //added by cheng 16/8------------------------------------------------
           break;
         case STATUS_FIX:
           fix->status.status = 0; // NavSatStatus::STATUS_FIX;
+          fix_str = "GPS get a normal fix"; //added by cheng 16/8------------------------------------------------
           break;
 // STATUS_DGPS_FIX was removed in API version 6 but re-added afterward
 #if GPSD_API_MAJOR_VERSION != 6 
         case STATUS_DGPS_FIX:
           fix->status.status = 2; // NavSatStatus::STATUS_GBAS_FIX;
+          fix_str = "DGPS fix";             //added by cheng 16/8------------------------------------------------
           break;
 #endif
       }
+      // ROS_INFO("fix->status.status: %d", fix->status.status);
 
       fix->status.service = NavSatStatus::SERVICE_GPS;
 
@@ -226,9 +281,25 @@ class GPSDClient {
        * as long as there has been a fix previously. Throw out these
        * fake results, which have NaN variance
        */
+
+      /* do not publish msg if there is no fix
+       */
+
+      // if (isnan(p->fix.epx) && check_fix_by_variance) {
+      //   return;
+      // }
+
+//---------added by cheng16/8-----------------------------------------------------------------------------------
+      /* publish msg even if there is no fix */
+
       if (isnan(p->fix.epx) && check_fix_by_variance) {
-        return;
+        fix->status.status = -1;
+        fix_str = "GPS can't get a fix";
       }
+
+      ROS_INFO("%s", fix_str.c_str());             
+
+//---------------------------------------------------------------------------------------------------------------
 
       fix->position_covariance[0] = p->fix.epx;
       fix->position_covariance[4] = p->fix.epy;
@@ -240,19 +311,40 @@ class GPSDClient {
     }
 };
 
+//-------added by cheng16/8----------------------------------------
+void killall() {
+  system("sudo killall gpsd");
+}
+//-----------------------------------------------------------------
+
 int main(int argc, char ** argv) {
   ros::init(argc, argv, "gpsd_client");
 
   GPSDClient client;
 
-  if (!client.start())
+//-------added by cheng16/8----------------------------------------
+  client.run_gpsd();
+//-----------------------------------------------------------------
+
+  if (!client.start()) {
+    killall();                  //added by cheng16/8---------------
     return -1;
+  }
 
 
   while(ros::ok()) {
+    // ROS_INFO("1");
     ros::spinOnce();
+    // ROS_INFO("2");
     client.step();
-  }
+    // ROS_INFO("3");
+  }  
+
 
   client.stop();
+
+//-------added by cheng16/8----------------------------------------
+  killall();
+//-----------------------------------------------------------------
+  
 }
